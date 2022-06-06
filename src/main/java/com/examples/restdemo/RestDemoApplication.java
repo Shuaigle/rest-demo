67d119171d617eb42d767f1b1e0754affdafe8fc
Shuaigle <layard3@gmail.com>
2022-06-06 16:23:12 +0800

    updated put method

--- src/main/java/com/examples/restdemo/RestDemoApplication.java
+++ src/main/java/com/examples/restdemo/RestDemoApplication.java
@@ -2,6 +2,8 @@ package com.examples.restdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -46,7 +48,7 @@ class Coffee {
 }
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/coffees")
 class RestApiDemoController {
 	private List<Coffee> coffees = new ArrayList<>();
 
@@ -58,12 +60,12 @@ class RestApiDemoController {
 		));
 	}
 
-	@GetMapping("/coffees")
+	@GetMapping
 	Iterable<Coffee> getCoffees() {
 		return coffees;
 	}
 
-	@GetMapping("/coffees/{id}")
+	@GetMapping("/{id}")
 	Optional<Coffee> getCoffeeById(@PathVariable String id) {
 		for (Coffee c: coffees) {
 			if (c.getId().equals(id)) {
@@ -75,16 +77,30 @@ class RestApiDemoController {
 	}
 
 	// spring boot catch Coffee object then transit it to JSON type and return it to request side with default
-	@PostMapping("/coffees")
+	@PostMapping
 	Coffee postCoffee(@RequestBody Coffee coffee) {
 		coffees.add(coffee);
 		return coffee;
 	}
 
 	// update coffee if it existed, otherwise create it
-	@PutMapping("/coffees/{id}")
-	Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-		// if not found, coffeeIndex = -1
+	//	@PutMapping("/{id}")
+	//	Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+	//		// if not found, coffeeIndex = -1
+	//		int coffeeIndex = -1;
+	//
+	//		for (Coffee c: coffees) {
+	//			if (c.getId().equals(id)) {
+	//				coffeeIndex = coffees.indexOf(c);
+	//				coffees.set(coffeeIndex, coffee);
+	//			}
+	//		}
+	//
+	//		return (coffeeIndex == -1) ? postCoffee(coffe) : coffee;
+	//	}
+
+	@PutMapping("/{id}")
+	ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
 		int coffeeIndex = -1;
 
 		for (Coffee c: coffees) {
@@ -94,10 +110,12 @@ class RestApiDemoController {
 			}
 		}
 
-		return (coffeeIndex == -1) ? postCoffee(coffee) : coffee;
+		return (coffeeIndex == -1) ?
+				new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
+				new ResponseEntity<>(coffee, HttpStatus.OK);
 	}
 
-	@DeleteMapping("/coffees/{id}")
+	@DeleteMapping("/{id}")
 	void deleteCoffee(@PathVariable String id) {
 		coffees.removeIf(c -> c.getId().equals(id));
 	}
